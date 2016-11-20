d26c7db2591c1acf768ea2faeece24adfb34b9e4
saitama-opman2015 <saitama.opman2015@gmail.com>
2016-11-20 22:25:44 +0530

    added values

---
+++ values/values.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("go" + "lang")
+	fmt.Println("1+1", 1+1)
+	fmt.Println("true && false ->", true && false)
+	fmt.Println("true || true ->", true || false)
+	fmt.Println("!true -> ", !true)
+}
